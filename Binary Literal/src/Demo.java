da92df6b1c4fa160803b159f41f28f7f8f95d60c
mackiie <muskaanmadan2000@gmail.com>
2020-06-02 23:56:28 +0530

    First commit

---
+++ Binary Literal/src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+
+	public static void main(String[] args) {
+		//This program is to convert the integer to Binary Number
+		int i = 0B101; // '0B' before the digits converts the integer number to decimal
+		
+		System.out.println(i);
+
+	}
+
+}
